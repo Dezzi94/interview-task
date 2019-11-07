76062677639c01e812bdd1f1ec2f99eb5cf0e527
Dezzi94 <techidesmond@gmail.com>
2019-11-07 16:28:18 +0000

    Add files via upload
    My last commit.

--- interview.js
+++ interview.js
@@ -1,4 +1,7 @@
-$(document).ready(function() { 
-	
 
-}
+$(document).ready(function() { 
+	alert("Hello there");
+
+// Plan was to use manipulate number class on span tag. Was going to use a loop and if statement to check what the number was and loop through outputing the text each time.
+//Decrease span class was going to have another loop to -1 each time.
+//If I had any extra time I would have put the code into a function and called the function.	
